eb8383715592b1853a8c4b8f337baf3269b82d7e
jackv24 <jackv949@gmail.com>
2017-01-21 12:51:47 +1030

    Character controller switches to physics from raycasts

--- Assets/Scripts/Characters/CharacterMove.cs
+++ Assets/Scripts/Characters/CharacterMove.cs
@@ -47,25 +47,20 @@ public class CharacterMove : MonoBehaviour
     public bool isGrounded = false;
 
     [Header("Physics")]
-    [Tooltip("How fast the character falls.")]
-    public float gravity = 10f;
     [Tooltip("The maximum speed at which the character can fall (otherwise known as terminal velocity).")]
     public float maxFallSpeed = 20f;
 
     [Space()]
-    [Tooltip("How many rays to cast down and up for collision detection.")]
-    public int verticalRays = 3;
-    [Tooltip("How many rays to cast left and right for collision detection.")]
-    public int horizontalRays = 5;
-    [Tooltip("How far inside the characters collider should the rays start from? (should be greater than 0)")]
-    public float skinWidth = 0.01f;
+    public LayerMask groundLayer;
 
     [Space()]
-    public LayerMask groundLayer;
-    public LayerMask ceilingLayer;
+    public Vector2 circleCastOrigin;
+    public float circleCastRadius = 1f;
+    public float groundedDistance = 0.1f;
 
     private Collider2D col;
     private Rect box;
+    private Rigidbody2D body;
 
     [HideInInspector]
     public Vector2 velocity;
@@ -74,20 +69,33 @@ public class CharacterMove : MonoBehaviour
     {
         //Get references
         col = GetComponent<Collider2D>();
+        body = GetComponent<Rigidbody2D>();
     }
 
     private void Update()
     {
-        //Store collider rect for easy typing
-        box = new Rect(
-            col.bounds.min.x,
-            col.bounds.min.y,
-            col.bounds.size.x,
-            col.bounds.size.y
-            );
+        //Get velocity, capping fall speed
+        velocity = body.velocity;
+        velocity.y = Mathf.Max(velocity.y, -maxFallSpeed);
 
-        //Apply gravity, capping fall speed
-        velocity.y = Mathf.Max(velocity.y - gravity * Time.deltaTime, -maxFallSpeed);
+        RaycastHit2D hit = Physics2D.CircleCast((Vector2)transform.position + circleCastOrigin, circleCastRadius, Vector2.down, Mathf.Infinity, groundLayer);
+
+        if (hit.collider != null)
+        {
+            Vector2 start = (Vector2)transform.position + circleCastOrigin;
+            start.y -= circleCastRadius;
+
+            float distance = Vector3.Distance(start, hit.point);
+
+            Debug.Log(distance);
+
+            if (distance <= groundedDistance)
+                isGrounded = true;
+            else
+                isGrounded = false;
+        }
+        else
+            isGrounded = false;
 
         //Jumping
         {
@@ -125,121 +133,13 @@ public class CharacterMove : MonoBehaviour
                 jumpHeldTime = jumpTime;
         }
 
-        //Vertical collision detection
-        {
-            //Calculate start and end points that rays will be cast from between
-            Vector2 startPoint = new Vector2(box.xMin + skinWidth, box.center.y);
-            Vector2 endPoint = new Vector2(box.xMax - skinWidth, box.center.y);
-
-            //Distance of rays should (when cast from centre) extend past the bottom by velocity amount (or skin width if grounded)
-            float distance = box.height / 2 + (isGrounded ? skinWidth : Mathf.Abs(velocity.y * Time.deltaTime));
-
-            //Not grounded unless a ray connects
-            isGrounded = false;
-
-            //All raycasts
-            RaycastHit2D[] hits = new RaycastHit2D[verticalRays];
-            bool connected = false;
-
-            //Keep track of the shortest ray
-            float minDistance = Mathf.Infinity;
-            int index = 0;
-
-            //Loops through and cast rays
-            for (int i = 0; i < verticalRays; i++)
-            {
-                //Get origin between start and end points
-                Vector2 origin = Vector2.Lerp(startPoint, endPoint, i / (float)(verticalRays - 1));
-
-                //Cast ray
-                hits[i] = Physics2D.Raycast(origin, (velocity.y > 0 ? Vector2.up : Vector2.down), distance, (velocity.y > 0 ? ceilingLayer : groundLayer));
-
-                Debug.DrawLine(origin, new Vector2(origin.x, origin.y + Mathf.Sign(velocity.y) * distance));
-
-                //If ray connected then player should be considered grounded
-                if (hits[i].collider != null)
-                {
-                    connected = true;
-
-                    //If this ray is shorter than any other, set it as the shortest and one to use
-                    if (hits[i].distance < minDistance)
-                    {
-                        minDistance = hits[i].distance;
-                        index = i;
-                    }
-                }
-            }
-
-            //If a ray has connected witht the ground
-            if (connected)
-            {
-                //Set grounded and stop falling (if already falling)
-                if (velocity.y <= 0)
-                    isGrounded = true;
-                else //If moving upwards, stop jumping
-                    heldJump = false;
-                velocity.y = 0;
-
-                //Move player flush to ground (using shortest ray)
-                transform.Translate(Vector2.down * (hits[index].distance - box.height / 2));
-            }
-        }
-
         //Horizontal movement
         if (canMove)
             velocity.x = Mathf.Lerp(velocity.x, moveSpeed * inputDirection, acceleration * Time.deltaTime);
 
-        //Lateral collision detection
-        {
-            //Only required if there is lateral movement
-            if (velocity.x != 0)
-            {
-                //Start and end points that ray origins will lay between
-                Vector2 startPoint = new Vector2(box.center.x, box.yMin + skinWidth);
-                Vector2 endPoint = new Vector2(box.center.x, box.yMax - skinWidth);
-
-                //Rays are cast out according to velocity, from the center
-                float distance = box.width / 2 + Mathf.Abs(velocity.x * Time.deltaTime);
-
-                //Rays are cast in the direction of movement
-                Vector2 direction = velocity.x > 0 ? Vector2.right : Vector2.left;
-
-                //Cast required amount of rays
-                for (int i = 0; i < horizontalRays; i++)
-                {
-                    //Calculate origin for this ray
-                    Vector2 origin = Vector2.Lerp(startPoint, endPoint, i / (float)(horizontalRays - 1));
-
-                    //Cast ray
-                    RaycastHit2D hit = Physics2D.Raycast(origin, direction, distance, groundLayer);
-
-                    Debug.DrawLine(origin, new Vector2(origin.x + distance * direction.x, origin.y));
-
-                    //If ray hit, there is a wall
-                    if (hit.collider != null)
-                    {
-                        //Calculate angle of slope
-                        float angle = Vector2.Angle(Vector2.up, hit.normal);
-
-                        //This is only considered a wall if outside slope limit
-                        if (angle > slopeLimit)
-                        {
-                            //Make character flush against wall
-                            transform.Translate(direction * (hit.distance - box.width / 2));
-
-                            //Cease any lateral movement
-                            velocity.x = 0;
-                        }
-
-                        //If one ray has connected, no more rays should be cast
-                        break;
-                    }
-                }
-            }
-        }
-
         //Move character by velocity
-        transform.Translate(velocity * Time.deltaTime);
+        //transform.Translate(velocity * Time.deltaTime);
+        body.velocity = velocity;
     }
 
 
@@ -261,5 +161,10 @@ public class CharacterMove : MonoBehaviour
             shouldJump = true;
 
         heldJump = pressed;
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.DrawWireSphere((Vector2)transform.position + circleCastOrigin, circleCastRadius);
     }
 }
